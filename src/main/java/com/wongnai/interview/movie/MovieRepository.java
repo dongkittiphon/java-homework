5b2bbc02951c49e00d9c54b9220cbbc810386e81
dongkittiphon <dd_dong.bk@hotmail.com>
2019-04-18 02:13:52 +0700

    #3 done(add case insensitive)

--- src/main/java/com/wongnai/interview/movie/MovieRepository.java
+++ src/main/java/com/wongnai/interview/movie/MovieRepository.java
@@ -9,19 +9,19 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, Long> {
-	/**
-	 * Find movies from name using a given keyword.
-	 * <p>
-	 * The underlying database, HSQLDB, is store in data in case sensitive manner,
-	 * so LIKE operation in the query below will also compare with case sensitive
-	 * which may differ from another RDBMS such as MySQL, SQLServer.
-	 * <p>
-	 * Please check case sensitivity carefully.
-	 *
-	 * @param keyword
-	 * 		a user query keyword
-	 * @return list of movie
-	 */
-	@Query("SELECT m FROM Movie m where m.name LIKE %:keyword%")
-	List<Movie> findByNameContains(@Param("keyword") String keyword);
+    /**
+     * Find movies from name using a given keyword.
+     * <p>
+     * The underlying database, HSQLDB, is store in data in case sensitive manner,
+     * so LIKE operation in the query below will also compare with case sensitive
+     * which may differ from another RDBMS such as MySQL, SQLServer.
+     * <p>
+     * Please check case sensitivity carefully.
+     *
+     * @param keyword
+     *      a user query keyword
+     * @return list of movie
+     */
+    @Query("SELECT m FROM Movie m where LOWER(m.name) LIKE %:keyword%")
+    List<Movie> findByNameContains(@Param("keyword") String keyword);
 }
